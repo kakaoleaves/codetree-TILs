04ea900e584606839ba10cb5089b5f6baa5701d7
Hanyoung Lee <kakaoleaves@naver.com>
2024-04-08 13:22:07 +0900

    Title: 코드트리 오마카세, Time: 1561ms, Memory: 10MB, Status: Time Limit Exceed - CodeTree

--- 240408/코드트리 오마카세/codetree-omakase.cpp
+++ 240408/코드트리 오마카세/codetree-omakase.cpp
@@ -25,8 +25,8 @@ vector<Command> commands;
 int cur_cmd = 0;
 int cur_t = 0;
 
-int main() {
-    // 여기에 코드를 작성해주세요.
+void Input()
+{
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
 
@@ -58,6 +58,139 @@ int main() {
 
         commands[i] = command;
     }
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    Input();
+
+    while (cur_cmd < q)
+    {
+        // 시간 동안 손님이 먹은 것 계산
+        if (!sushis.empty() && !customers.empty())
+        {
+            vector<string> customersToErase;
+            for (auto it = customers.begin(); it != customers.end(); it++)
+            {
+                auto& [name, cinfo] = *it;
+                auto& [cx, cn] = cinfo;
+                auto [start, end] = sushis.equal_range(name);
+                
+                // 손님이 먼저 닿을 수 있는 스시 순서대로 정렬한다.
+                vector<unordered_multimap<string, pair<int, int>>::iterator> _sushis;
+                vector<pair<int, int>> csx_sushis; // {csx, idx}
+
+                for (auto itt = start; itt != end; itt++)
+                {
+					const auto& [_, sinfo] = *itt;
+					const auto& [sx, st] = sinfo;
+					int csx = (cur_t - st + sx) % l;
+                    _sushis.push_back(itt);
+                    csx_sushis.push_back({ (cx - csx + l) % l, _sushis.size() - 1});
+				}
+
+                sort(csx_sushis.begin(), csx_sushis.end());
+
+                int timeGap = (commands[cur_cmd].t - cur_t);
+
+                for (auto& csx_sushi : csx_sushis)
+                {
+                    const auto& [csx, idx] = csx_sushi;
+                    if (csx - timeGap <= 0)
+                    {
+                        sushis.erase(_sushis[idx]);
+                        cn--;
+                        if (cn == 0) break;
+                    }
+                    else break;
+                }
+
+                if (cn == 0) customersToErase.push_back(name);
+            }
+
+            for (const auto& name : customersToErase)
+            {
+				customers.erase(name);
+			}
+        }
+
+        cur_t = commands[cur_cmd].t;
+
+        // 명령 처리
+        if (Command& cur_command = commands[cur_cmd]; cur_command.cmd == 300)
+        {
+            cout << customers.size() << ' ' << sushis.size() << '\n';
+        }
+        else if (cur_command.cmd == 200)
+        {
+            auto [cmd, t, x, name, n] = cur_command;
+            // 이름이 같은 초밥을 찾는다.
+            auto [start, end] = sushis.equal_range(name);
+            vector<unordered_multimap<string, pair<int, int>>::iterator> toErase;
+            for (auto it = start; it != end; it++)
+            {
+                const auto& [_, sinfo] = *it;
+                const auto& [sx, st] = sinfo;
+
+                int csx = (cur_t - st + sx) % l;
+                if (csx == x)
+                {
+                    // cout << cur_t << ": " << name << "가 먹었다!" << endl;
+                    toErase.push_back(it);
+                    n--;
+                    if (n == 0) break;
+                }
+            }
+
+            for (const auto& it : toErase)
+            {
+                sushis.erase(it);
+            }
+
+            if (n > 0)
+                customers.try_emplace(name, x, n);
+        }
+        else if (cur_command.cmd == 100)
+        {
+            const string& name = cur_command.name;
+
+            if (auto it = customers.find(name); it != customers.end())
+            {
+                auto& [_, cinfo] = *it;
+                auto& [cx, cn] = cinfo;
+
+                if (cx == cur_command.x)
+                {
+                    cn--;
+                    // cout << cur_t << ": " << name << "가 먹었다!" << endl;
+                    if (cn == 0) customers.erase(name);
+                }
+                else
+                {
+                    sushis.insert({
+                        name,
+                        {
+                            cur_command.x,
+                            cur_t
+                        }
+                        });
+                }
+            }
+            else
+            {
+                sushis.insert({
+                name,
+                {
+                   cur_command.x,
+                    cur_t
+                }
+                    });
+            }
+        }
+        cur_cmd++;
+    }
+
+
 
     while (cur_cmd < q)
     {
@@ -82,7 +215,7 @@ int main() {
                 if (csx == cx)
                 {
                     cn--;
-                    // cout << cur_t << ": " << name << "가 먹었다!" << endl;
+                    cout << cur_t << ": " << name << "가 먹었다!" << endl;
                     sushisToErase.push_back(itt);
 
                     if (cn == 0)
@@ -104,81 +237,7 @@ int main() {
 			customers.erase(name);
 		}
 
-
-        if (commands[cur_cmd].t == cur_t)
-        {
-            if (Command& cur_command = commands[cur_cmd]; cur_command.cmd == 300)
-            {
-                cout << customers.size() << ' ' << sushis.size() << '\n';
-            }
-            else if (cur_command.cmd == 200)
-            {
-                auto [cmd, t, x, name, n] = cur_command;
-                // 이름이 같은 초밥을 찾는다.
-                auto [start, end] = sushis.equal_range(name);
-                vector<unordered_multimap<string, pair<int, int>>::iterator> toErase;
-                for (auto it = start; it != end; it++)
-                {
-                    const auto& [_, sinfo] = *it;
-                    const auto& [sx, st] = sinfo;
-
-                    int csx = (cur_t - st + sx) % l;
-                    if (csx == x)
-                    {
-						// cout << cur_t << ": " << name << "가 먹었다!" << endl;
-                        toErase.push_back(it);
-                        n--;
-						if (n == 0) break;
-					}
-                }
-
-                for (const auto& it : toErase)
-                {
-					sushis.erase(it);
-				}
-
-				if (n > 0)
-                    customers.try_emplace(name, x, n);
-            }
-            else if (cur_command.cmd == 100)
-            {
-                const string& name = cur_command.name;
-
-                if (auto it = customers.find(name); it != customers.end())
-                {
-                    auto& [_, cinfo] = *it;
-                    auto& [cx, cn] = cinfo;
-
-                    if (cx == cur_command.x)
-                    {
-                        cn--;
-                        // cout << cur_t << ": " << name << "가 먹었다!" << endl;
-                        if (cn == 0) customers.erase(name);
-                    }
-                    else
-                    {
-                        sushis.insert({
-                            name,
-                            {
-                                cur_command.x,
-                                cur_t
-                            }
-                            });
-                    }
-                }
-                else
-                {
-                    sushis.insert({
-                    name,
-                    {
-                       cur_command.x,
-                        cur_t
-                    }
-                    });
-                }
-            }
-            cur_cmd++;
-        }
+       
     }
 
 
