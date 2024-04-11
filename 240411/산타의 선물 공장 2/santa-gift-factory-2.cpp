b1fb1f73f33dfc127d63d43a42a9695432d83d99
Hanyoung Lee <kakaoleaves@naver.com>
2024-04-11 21:22:40 +0900

    Title: 산타의 선물 공장 2, Time: 7ms, Memory: 2MB, Status: Wrong Answer - CodeTree

--- 240411/산타의 선물 공장 2/santa-gift-factory-2.cpp
+++ 240411/산타의 선물 공장 2/santa-gift-factory-2.cpp
@@ -13,14 +13,21 @@ int m; // 물건의 수; 1 <= m <= 100'000
 
 vector<int> heads(MAX_N, -1); // 각 벨트의 head
 vector<int> tails(MAX_N, -1); // 각 벨트의 tail
+vector<int> counts(MAX_N, 0); // 각 벨트에 존재하는 개수 표기
 
 vector<int> prv(MAX_M, -1); // 각 물건의 이전
 vector<int> nxt(MAX_M, -1); // 각 물건의 이후
-vector<int> counts(MAX_M, 0); // 각 벨트에 존재하는 개수 표기
 
 void BuildFactory()
 {
     cin >> n >> m;
+
+    heads.resize(n + 1, -1);
+    tails.resize(n + 1, -1);
+    counts.resize(n + 1, 0);
+
+    prv.resize(m + 1, -1);
+    nxt.resize(m + 1, -1);
 
     for (int id = 1; id <= m; id++)
     {
@@ -63,37 +70,33 @@ void MoveAll()
 
     // 아니라면 m_src에 있는 모든 물건을 m_dst 앞으로 옮겨야함
 
-    // 1. head 변경 2. tail 변경 4. count 변경
-
-    // dst의 head, tail에 대한 작업
-    // m_dst에 물건이 없었다면, head와 tail이 그대로 따라옴
+    // dst가 비어있다면
     if (heads[m_dst] == -1)
     {
-		tails[m_dst] = tails[m_src];
         heads[m_dst] = heads[m_src];
+		tails[m_dst] = tails[m_src];
     }
 
-    // dst에 물건이 있었다면, src의 tail과 dst의 head를 연결 
+    // dst가 비어있지 않다면
     else
     {
-        // src의 tail과 dst의 head를 연결
-        int cur_dst_head = heads[m_dst];
-        int cur_src_tail = tails[m_src];
+		// src의 tail을 dst의 head 앞에 연결
+		nxt[tails[m_src]] = heads[m_dst];
+		prv[heads[m_dst]] = tails[m_src];
 
-		nxt[cur_src_tail] = cur_dst_head;
-        prv[cur_dst_head] = cur_src_tail;
+		// dst의 head 변경
+		heads[m_dst] = heads[m_src];
+	}
 
-        heads[m_dst] = heads[m_src];
-    }
+    // src의 head와 tail 초기화
+	heads[m_src] = -1;
+	tails[m_src] = -1;
 
-    // src의 head, tail 변경
-    heads[m_src] = tails[m_src] = -1;
+	// count 갱신
+	counts[m_dst] += counts[m_src];
+	counts[m_src] = 0;
 
-    // count 변경
-    counts[m_dst] += counts[m_src];
-    counts[m_src] = 0;
-
-    cout << counts[m_dst] << endl;
+	cout << counts[m_dst] << endl;
 }
 
 // src, dst의 head끼리 swap
@@ -116,13 +119,14 @@ void SwapFront()
         heads[m_src] = tails[m_src] = heads[m_dst];
 
         // dst의 head는 기존 head의 다음이 된다
-
         // 만약 dst의 head가 tail이라면 tail도 변경
         if (heads[m_dst] == tails[m_dst])
         {
             heads[m_dst] = -1;
             tails[m_dst] = -1;
-		}
+            // prv, nxt는 변경할 필요 X
+        }
+        // 아니라면 head만 변경
         else
         {
             heads[m_dst] = nxt[heads[m_dst]];
@@ -131,7 +135,7 @@ void SwapFront()
 
         // src의 head가 되었으므로 이전, 이후 모두 없음
         nxt[heads[m_src]] = -1;
-
+        prv[heads[m_src]] = -1;
 
         // count 변경
         counts[m_src]++;
@@ -141,33 +145,32 @@ void SwapFront()
     // dst가 비어있는 경우
     else if (heads[m_dst] == -1)
     {
-		// src의 head가 dst로 이동한다
+        // src의 head가 dst로 이동한다
         heads[m_dst] = tails[m_dst] = heads[m_src];
 
         // src의 head는 기존 head의 다음이 된다
-
         // 만약 src의 head가 tail이라면 tail도 변경
         if (heads[m_src] == tails[m_src])
         {
-			heads[m_src] = -1;
-			tails[m_src] = -1;
-		}
+            heads[m_src] = -1;
+            tails[m_src] = -1;
+            // prv, nxt는 변경할 필요 X
+        }
+        // 아니라면 head만 변경
         else
         {
-			heads[m_src] = nxt[heads[m_src]];
-			prv[heads[m_src]] = -1; // head가 되었으므로 이전은 없음
-		}
+            heads[m_src] = nxt[heads[m_src]];
+            prv[heads[m_src]] = -1; // head가 되었으므로 이전은 없음
+        }
 
         // dst의 head가 되었으므로 이전, 이후 모두 없음
         nxt[heads[m_dst]] = -1;
-
-        // 소속 변경 (하나만 해도 된다)
-        int cur = heads[m_dst];
+        prv[heads[m_dst]] = -1;
 
         // count 변경
         counts[m_src]--;
         counts[m_dst]++;
-	}
+    }
 
     // 둘 다 비어있지 않은 경우
     else
@@ -175,30 +178,28 @@ void SwapFront()
         int src_head = heads[m_src];
         int dst_head = heads[m_dst];
 
-        // 서로의 head 변경
+        // 서로의 head를 swap
         heads[m_src] = dst_head;
         heads[m_dst] = src_head;
 
-        // 서로의 head의 이후 변경 (이전은 변경할 필요 없음)
-        int src_nxt = nxt[src_head];
+        // src의 head가 tail이라면 tail도 변경
+        if (src_head == tails[m_src])
+        {
+			tails[m_src] = dst_head;
+		}
+
+        // dst의 head가 tail이라면 tail도 변경
+		if (dst_head == tails[m_dst])
+		{
+            tails[m_dst] = src_head;
+        }
+
+        // 서로 head의 이후 변경
+		int src_nxt = nxt[src_head];
         int dst_nxt = nxt[dst_head];
 
         nxt[src_head] = dst_nxt;
         nxt[dst_head] = src_nxt;
-
-        // 서로의 head가 tail이라면 tail도 변경
-        if (src_head == tails[m_src])
-        {
-			tails[m_src] = dst_head;
-            nxt[dst_head] = -1; // tail이 되었으므로 이후는 X
-        }
-
-        if (dst_head == tails[m_dst])
-		{
-            tails[m_dst] = src_head;
-            nxt[src_head] = -1; // tail이 되었으므로 이후는 X
-        }
-        // count는 그대로
     }
     
     cout << counts[m_dst] << endl;
@@ -219,39 +220,39 @@ void Divide()
     // 옮겨질 개수
     int cnt = counts[m_src] / 2;
 
-    // 개수만큼 count 갱신
-    counts[m_dst] += cnt;
+    // 개수 먼저 갱신
     counts[m_src] -= cnt;
+    counts[m_dst] += cnt;
 
-    // 최종 cur은 floor(n / 2) 다음의 물건, 즉 이 부분이 src의 head가 된다.
     int cur = heads[m_src];
+
+    // src의 head를 cnt만큼 dst로 옮김
     for (int i = 0; i < cnt; i++)
     {
-		cur = nxt[cur];
-	}
+        cur = nxt[cur];
+    }
 
-    // dst의 head 변경
-    // dst가 비어있다면 head와 tail 모두 변경
+    // dst의 head가 비어있다면
     if (heads[m_dst] == -1)
     {
-        heads[m_dst] = heads[m_src];
-        tails[m_dst] = prv[cur];        
-        nxt[prv[cur]] = -1; // tail이 되었으므로 이후는 X
-	}
+		heads[m_dst] = heads[m_src];
+        tails[m_dst] = prv[cur];
+    }
 
-    // dst가 비어있지 않다면
+    // dst의 head가 비어있지 않다면
     else
     {
-        // 옮긴 src의 head는 dst의 head
-        // 옮긴 src의 tail은 dst의 head와 연결
-        nxt[prv[cur]] = heads[m_dst];
-        prv[heads[m_dst]] = prv[cur];
-        heads[m_dst] = heads[m_src];
-	}
+		// src의 tail을 dst의 head 앞에 연결
+		nxt[prv[cur]] = heads[m_dst];
+		prv[heads[m_dst]] = prv[cur];
 
-    // src의 head는 cur
+		// dst의 head 변경
+		heads[m_dst] = heads[m_src];
+    }
+
+    // src의 head 변경
     heads[m_src] = cur;
-    prv[cur] = -1; // head가 되었으므로 이전은 없음    
+    prv[cur] = -1;
 
     cout << counts[m_dst] << endl;
 }
